6aad057b9fe1136c811cff2aeba1d594274d1b8b
Brandon Valosek <bvalosek@gmail.com>
2022-10-26 15:58:51 -0500

    index new account transfer / recovery stuff

--- src/account-registry.ts
+++ src/account-registry.ts
@@ -1,5 +1,11 @@
-import { AccountCreated } from "../generated/AccountRegistryDataSource/AccountRegistry";
+import {
+  AccountBroadcast,
+  AccountCreated,
+  AccountRecoverySet,
+  AccountTransfered,
+} from "../generated/AccountRegistryDataSource/AccountRegistry";
 import { Account } from "../generated/schema";
+import { getAccount } from "./entities";
 
 export function handleAccountCreated(event: AccountCreated): void {
   const timestamp = event.block.timestamp;
@@ -11,5 +17,26 @@ export function handleAccountCreated(event: AccountCreated): void {
   account.recoveryAddress = event.params.recovery.toHexString();
   account.createdAtTimestamp = timestamp;
   account.metadata = event.params.metadata;
+  account.save();
+}
+
+export function handleAccountBroadcast(event: AccountBroadcast): void {
+  const account = getAccount(event.params.id);
+
+  if (event.params.topic.localeCompare("metadata") === 0) {
+    account.metadata = event.params.message;
+    account.save();
+  }
+}
+
+export function handleAccountTransfered(event: AccountTransfered): void {
+  const account = getAccount(event.params.id);
+  account.address = event.params.newOwner.toHexString();
+  account.save();
+}
+
+export function handleAccountRecoverySet(event: AccountRecoverySet): void {
+  const account = getAccount(event.params.id);
+  account.recoveryAddress = event.params.newRecoveryAddress.toHexString();
   account.save();
 }
