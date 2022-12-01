d60f4571cd4f1413cbbc16250bd994c9afb83b86
Brandon Valosek <bvalosek@gmail.com>
2022-12-01 10:25:52 -0600

    update drop engine indexing

--- src/drop-engine.ts
+++ src/drop-engine.ts
@@ -1,4 +1,8 @@
-import { DropCreated } from "../generated/DropEngineDataSource/DropEngine";
+import { Address } from "@graphprotocol/graph-ts";
+import {
+  DropCreated,
+  PublicMintEnabled,
+} from "../generated/DropEngineDataSource/DropEngine";
 import { getCollection, getSequence } from "./entities";
 
 export function handleDropCreated(event: DropCreated): void {
@@ -8,5 +12,19 @@ export function handleDropCreated(event: DropCreated): void {
   sequence.revenueRecipient = event.params.recipient.toHexString();
   sequence.royaltyBps = event.params.royaltyBps;
   sequence.uriPrefix = event.params.uriPrefix;
+
+  sequence.isPublicMint = event.params.mintAuthority.equals(Address.zero());
+  if (!sequence.isPublicMint) {
+    sequence.mintAuthority = event.params.mintAuthority.toHexString();
+  }
+
+  sequence.save();
+}
+
+export function handlePublicMintEnabled(event: PublicMintEnabled): void {
+  const collection = getCollection(event.params.collection);
+  const sequence = getSequence(collection.id, event.params.sequenceId);
+  sequence.isPublicMint = true;
+  sequence.mintAuthority = null;
   sequence.save();
 }
