20e9e45b4899ede6120e4b8715a9c033a5969707
Brandon Valosek <bvalosek@gmail.com>
2023-02-14 14:19:11 -0600

    readding more stuff

--- src/collection-factory.ts
+++ src/collection-factory.ts
@@ -2,29 +2,30 @@ import { CollectionCreated } from "../generated/CollectionFactoryDataSource/Coll
 import { Collection as CollectionContract } from "../generated/templates/CollectionDataSource/Collection";
 import { Collection } from "../generated/schema";
 import { CollectionDataSource } from "../generated/templates";
-import { getNode } from "./entities";
+import { getNodeById } from "./entities";
 
 export function handleCollectionCreated(event: CollectionCreated): void {
   const timestamp = event.block.timestamp;
   const address = event.params.collection;
   const id = `collection-${address.toHexString()}`;
   const collection = new Collection(id);
-
   const collectionContract = CollectionContract.bind(address);
+  const node = getNodeById(collectionContract.controlNode().toString());
 
   collection.address = address.toHexString();
   collection.name = collectionContract.name();
   collection.symbol = collectionContract.symbol();
   collection.ownerAddress = collectionContract.owner().toHexString();
+  collection.controlNode = node.id;
   collection.recordCount = 0;
   collection.sequenceCount = 0;
   collection.createdAtTimestamp = timestamp;
   collection.createdAtTransaction = event.transaction.hash;
+  collection.save();
 
-  const controlNodeId = collectionContract.controlNode();
-  collection.controlNode = getNode(controlNodeId).id;
+  node.collectionCount++;
+  node.save();
 
   // save and spawn new datasource for this catalog
-  collection.save();
-  // CollectionDataSource.create(address);
+  CollectionDataSource.create(address);
 }
