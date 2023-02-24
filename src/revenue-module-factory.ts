6549e616560d548f9242c9f1239a81ebe7506bbc
Brandon Valosek <bvalosek@gmail.com>
2023-02-23 19:44:11 -0600

    rev module factory updates

--- src/revenue-module-factory.ts
+++ src/revenue-module-factory.ts
@@ -1,3 +1,4 @@
+import { Address } from "@graphprotocol/graph-ts";
 import { RevenueModuleDeployed } from "../generated/RevenueModuleFactoryDataSource/RevenueModuleFactory";
 import { RevenueModule } from "../generated/schema";
 import { getNode } from "./entities";
@@ -9,23 +10,34 @@ export function handleRevenueModuleDeployed(
   const waterfallAddress = event.params.waterfall;
   const splitAddress = event.params.split;
   const node = getNode(event.params.config.controlNodeId);
+  const address = waterfallAddress.equals(Address.zero())
+    ? splitAddress
+    : waterfallAddress;
 
-  const id = `revenue-module-${waterfallAddress.toHexString()}`;
+  const id = `revenue-module-${address.toHexString()}`;
   const revMod = new RevenueModule(id);
-  revMod.waterfallAddress = waterfallAddress.toHexString();
-  revMod.splitAddress = splitAddress.toHexString();
-  revMod.metadata = event.params.config.metadata;
+  revMod.address = address.toHexString();
   revMod.controlNode = node.id;
-  revMod.waterfallRecipientAddresses = event.params.config.waterfallRecipients.map<
-    string
-  >((a) => a.toHexString());
-  revMod.waterfallRecipientThresholds = event.params.config.waterfallThresholds;
-  revMod.splitRecipientAddresses = event.params.config.splitRecipients.map<
-    string
-  >((a) => a.toHexString());
-  revMod.splitRecipientAllocations = event.params.config.splitPercentAllocations.map<
-    i32
-  >((a) => a.toI32());
+  revMod.metadata = event.params.config.metadata;
+
+  if (!waterfallAddress.equals(Address.zero())) {
+    revMod.waterfallAddress = waterfallAddress.toHexString();
+    revMod.waterfallRecipientAddresses = event.params.config.waterfallRecipients.map<
+      string
+    >((a) => a.toHexString());
+    revMod.waterfallRecipientThresholds =
+      event.params.config.waterfallThresholds;
+  }
+  if (!splitAddress.equals(Address.zero())) {
+    revMod.splitAddress = splitAddress.toHexString();
+    revMod.splitRecipientAddresses = event.params.config.splitRecipients.map<
+      string
+    >((a) => a.toHexString());
+    revMod.splitRecipientAllocations = event.params.config.splitPercentAllocations.map<
+      i32
+    >((a) => a.toI32());
+  }
+
   revMod.createdAtTimestamp = timestamp;
   revMod.createdAtTransaction = event.transaction.hash;
   revMod.save();
